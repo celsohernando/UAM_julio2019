72a8c1c081edfc622058ad5f16acf13fb3383c41
celsohernando <celso.hernando@es.ibm.com>
2019-09-17 13:57:06 +0200

    Create dht11.py

---
+++ dht11.py
@@ -0,0 +1,15 @@
+import Adafruit_DHT
+import time
+import json
+
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 14
+
+while True:
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+      data = {'temperature': temperature, 'humidity': humidity}
+      print(json.dumps(data))
+    else:
+        print("Sensor failure. Check wiring.");
+    time.sleep(3)
